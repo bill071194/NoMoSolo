71b47f343df4b041b1fa4b930956b8ad186d16a5
umarsalih <salihumar13@gmail.com>
2022-11-17 17:53:30 -0800

    Commit first

---
+++ NoMoSoloApp/app/src/main/java/com/example/nomosoloapp/ResetPassword01.java
@@ -0,0 +1,14 @@
+package com.example.nomosoloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password01);
+    }
+}
